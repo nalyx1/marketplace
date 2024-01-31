468df8b75eb72d952f74700ad95e736bf906b95a
João Rodrigues <joaorodriguez0106@gmail.com>
2024-01-31 13:20:53 -0300

    feat: product and category entities

---
+++ go/internal/entity/entity.go
@@ -0,0 +1,36 @@
+package entity
+
+import "github.com/google/uuid"
+
+type Category struct {
+	ID          string    `json:"id"`
+	Name        string `json:"name"`
+}
+
+func NewCategory(name string) *Category {
+	return &Category{
+		ID:          uuid.New().String(),
+		Name:        name,
+	}
+}
+
+type Product struct {
+	ID          string    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       float64 `json:"price"`
+	CategoryID    string `json:"category"`
+	ImageURL       string `json:"image"`
+
+}
+
+func NewProduct(name string, description string, price float64, categoryID string, imageURL string) *Product {
+	return &Product{
+		ID:          uuid.New().String(),
+		Name:        name,
+		Description: description,
+		Price:       price,
+		CategoryID:  categoryID,
+		ImageURL:    imageURL,
+	}
+}
